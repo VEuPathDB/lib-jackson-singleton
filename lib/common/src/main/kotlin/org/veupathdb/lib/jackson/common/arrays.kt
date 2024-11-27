abc21dd72357091919eeb4e81671ee0021e98f84
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-11-27 11:51:40 -0500

    add public dependencies

--- lib/common/src/main/kotlin/org/veupathdb/lib/jackson/common/arrays.kt
+++ lib/common/src/main/kotlin/org/veupathdb/lib/jackson/common/arrays.kt
@@ -4,8 +4,6 @@ package org.veupathdb.lib.jackson.common
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.module.kotlin.contains
 import java.math.BigDecimal
 import java.math.BigInteger
 import kotlin.math.max
