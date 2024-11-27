06a0c1539e044e5fbdc94772e3184bc2b2c5aa7b
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-11-27 11:51:40 -0500

    add public dependencies

--- lib/json/build.gradle.kts
+++ lib/json/build.gradle.kts
@@ -1,3 +1,11 @@
 dependencies {
   implementation(project(":lib:jackson-singleton-common"))
+
+  api(platform(libs.jackson))
+  api("com.fasterxml.jackson.core:jackson-databind")
+  api("com.fasterxml.jackson.datatype:jackson-datatype-json-org")
+  api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+  api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+  api("com.fasterxml.jackson.module:jackson-module-kotlin")
+  api("com.fasterxml.jackson.module:jackson-module-parameter-names")
 }
